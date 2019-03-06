e96f911e7b29b88dd43d89d2bafc7649a885fa40
Luke Vo <datvm@outlook.com>
2019-03-07 05:03:44 +0700

    Add project files.

---
+++ TurboXInput.Core/XInputInvoker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TurboXInput.Core
+{
+
+    public static class XInputInvoker
+    {
+        private const string XInputDllName = "XINPUT1_4.DLL";
+
+        [DllImport(XInputDllName)]
+        public static extern void XInputEnable(bool enabled);
+
+        [DllImport(XInputDllName)]
+        public static extern XInputGetCapabilitiesResult XInputGetCapabilities(uint dwUserIndex, XInputGetCapabilitiesFlag dwFlags, ref XInputCapabilities pCapabilities);
+
+    }
+
+    [Flags]
+    public enum XInputGetCapabilitiesResult : uint
+    {
+        ERROR_SUCCESS = 0,
+        ERROR_DEVICE_NOT_CONNECTED = 0x48F,
+    }
+
+    [Flags]
+    public enum XInputGetCapabilitiesFlag : uint
+    {
+        XINPUT_FLAG_GAMEPAD,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct XInputCapabilities
+    {
+        public byte Type;
+        public byte SubType;
+        public ushort Flags;
+        public XInputGamepad Gamepad;
+        public XInputVibration Vibration;
+
+        [Flags]
+        public enum XInputCapabilitiesFlag
+        {
+            XINPUT_CAPS_VOICE_SUPPORTED,
+            XINPUT_CAPS_FFB_SUPPORTED,
+            XINPUT_CAPS_WIRELESS,
+            XINPUT_CAPS_PMD_SUPPORTED,
+            XINPUT_CAPS_NO_NAVIGATION,
+        }
+
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct XInputGamepad
+    {
+        public XInputGamepadFlag wButtons;
+        byte bLeftTrigger;
+        byte bRightTrigger;
+        short sThumbLX;
+        short sThumbLY;
+        short sThumbRX;
+        short sThumbRY;
+
+        [Flags]
+        public enum XInputGamepadFlag
+        {
+            DPadUp = 0x00000001,
+            DPadDown = 0x00000002,
+            DPadLeft = 0x00000004,
+            DPadRight = 0x00000008,
+            Start = 0x00000010,
+            Back = 0x00000020,
+            LeftThumb = 0x00000040,
+            RightThumb = 0x00000080,
+            LeftShoulder = 0x0100,
+            RightShoulder = 0x0200,
+            Guide = 0x0400,
+            A = 0x1000,
+            B = 0x2000,
+            X = 0x4000,
+            Y = 0x8000
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct XInputVibration
+    {
+        ushort wLeftMotorSpeed;
+        ushort wRightMotorSpeed;
+    }
+
+}
---
+++ TurboXInput.Test/XInputInvokerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TurboXInput.Core;
+using Xunit;
+
+namespace TurboXInput.Test
+{
+
+    public class XInputInvokerTest
+    {
+
+        [Fact]
+        public void ShouldEnableAppXInput()
+        {
+            XInputInvoker.XInputEnable(true);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public void ShouldGetControllerCapacity(uint index)
+        {
+            var result = new XInputCapabilities();
+            var opResult = XInputInvoker.XInputGetCapabilities(index, XInputGetCapabilitiesFlag.XINPUT_FLAG_GAMEPAD, ref result);
+
+            Assert.Equal(XInputGetCapabilitiesResult.ERROR_SUCCESS, opResult);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void ShouldNotGetControllerCapacity(uint index)
+        {
+            var result = new XInputCapabilities();
+            var opResult = XInputInvoker.XInputGetCapabilities(index, XInputGetCapabilitiesFlag.XINPUT_FLAG_GAMEPAD, ref result);
+
+            Assert.Equal(XInputGetCapabilitiesResult.ERROR_DEVICE_NOT_CONNECTED, opResult);
+        }
+
+    }
+
+}
